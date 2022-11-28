aa44c63b031d0c009e1b7219e277ae0a612bc68f
Peter Toth <peter.t.toth92@gmail.com>
2022-11-28 21:00:12 +0100

    added script/   debian and win64 build scripts

---
+++ script/build_debian.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#find out script directory
+pushd $(dirname "${BASH_SOURCE[0]}") > /dev/null
+scriptdir=$(pwd)
+popd > /dev/null
+
+#move to project root directory
+pushd $scriptdir/..
+
+builddir="./build/debian"
+cmake ./projects/debian -B$builddir -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+pushd $builddir
+
+#get available core count
+cores=$(nproc)
+
+make -j $cores
+
+popd #from $builddir
+
+popd #from $scriptdir
---
+++ script/build_win64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#find out script directory
+pushd $(dirname "${BASH_SOURCE[0]}") > /dev/null
+scriptdir=$(pwd)
+popd > /dev/null
+
+#move to project root directory
+pushd $scriptdir/..
+
+builddir="./build/win64"
+cmake ./projects/win64 -B$builddir -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+pushd $builddir
+
+#get available core count
+cores=$(nproc)
+
+make -j $cores
+
+popd #from $builddir
+
+popd #from $scriptdir
